6ad1df4cb83b8cfe4d8f706015be2911216c31d4
Igal Alkon <igal.alkon@gmail.com>
2018-05-22 16:33:50 +0300

    now we install packages after making them

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,8 +13,24 @@ dnf builddep -y spec/magnum-extras.spec
 dnf builddep -y spec/magnum-examples.spec
 
 rpmbuild --undefine=_disable_source_fetch --define "debug_package %{nil}" -ba spec/corrade.spec
+dnf install -y ~/rpmbuild/RPMS/x86_64/corrade-2018.04-1.x86_64.rpm
+dnf install -y ~/rpmbuild/RPMS/x86_64/corrade-devel-2018.04-1.x86_64.rpm
+
 rpmbuild --undefine=_disable_source_fetch --define "debug_package %{nil}" -ba spec/magnum.spec
+dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-2018.04-1.x86_64.rpm
+dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-devel-2018.04-1.x86_64.rpm
+
 rpmbuild --undefine=_disable_source_fetch --define "debug_package %{nil}" -ba spec/magnum-plugins.spec
+dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-plugins-2018.04-1.x86_64.rpm
+dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-plugins-devel-2018.04-1.x86_64.rpm
+
 rpmbuild --undefine=_disable_source_fetch --define "debug_package %{nil}" -ba spec/magnum-integration.spec
+dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-plugins-2018.04-1.x86_64.rpm
+dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-plugins-devel-2018.04-1.x86_64.rpm
+
 rpmbuild --undefine=_disable_source_fetch --define "debug_package %{nil}" -ba spec/magnum-extras.spec
+dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-extras-2018.04-1.x86_64.rpm
+dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-extras-devel-2018.04-1.x86_64.rpm
+
 rpmbuild --undefine=_disable_source_fetch --define "debug_package %{nil}" -ba spec/magnum-examples.spec
+dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-examples-2018.04-1.x86_64.rpm
