96c4a1985108784a77538496d2549288abffbf99
Igal Alkon <igal.alkon@gmail.com>
2018-05-22 16:15:10 +0300

    adding rpm build tools to bootstrap, remove comfirm on dnf

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,13 +3,14 @@
 # RUN AS ROOT USER!
 
 dnf group install -y 'Development Tools'
+dnf install -y fedora-packager rpmdevtools
 
-dnf builddep spec/corrade.spec
-dnf builddep spec/magnum.spec
-dnf builddep spec/magnum-plugins.spec
-dnf builddep spec/magnum-integration.spec
-dnf builddep spec/magnum-extras.spec
-dnf builddep spec/magnum-examples.spec
+dnf builddep -y spec/corrade.spec
+dnf builddep -y spec/magnum.spec
+dnf builddep -y spec/magnum-plugins.spec
+dnf builddep -y spec/magnum-integration.spec
+dnf builddep -y spec/magnum-extras.spec
+dnf builddep -y spec/magnum-examples.spec
 
 rpmbuild --undefine=_disable_source_fetch --define "debug_package %{nil}" -ba spec/corrade.spec
 rpmbuild --undefine=_disable_source_fetch --define "debug_package %{nil}" -ba spec/magnum.spec
