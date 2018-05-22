1d8c96cc87010dfc1cd6465da9351614e7a2e4c9
Igal Alkon <igal.alkon@gmail.com>
2018-05-22 16:46:31 +0300

    fix and clean build

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,22 +15,28 @@ dnf builddep -y spec/magnum-examples.spec
 rpmbuild --undefine=_disable_source_fetch --define "debug_package %{nil}" -ba spec/corrade.spec
 dnf install -y ~/rpmbuild/RPMS/x86_64/corrade-2018.04-1.x86_64.rpm
 dnf install -y ~/rpmbuild/RPMS/x86_64/corrade-devel-2018.04-1.x86_64.rpm
+rpmbuild --clean spec/corrade.spec
 
 rpmbuild --undefine=_disable_source_fetch --define "debug_package %{nil}" -ba spec/magnum.spec
 dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-2018.04-1.x86_64.rpm
 dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-devel-2018.04-1.x86_64.rpm
+rpmbuild --clean spec/magnum.spec
 
 rpmbuild --undefine=_disable_source_fetch --define "debug_package %{nil}" -ba spec/magnum-plugins.spec
 dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-plugins-2018.04-1.x86_64.rpm
 dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-plugins-devel-2018.04-1.x86_64.rpm
+rpmbuild --clean spec/magnum-plugins.spec
 
 rpmbuild --undefine=_disable_source_fetch --define "debug_package %{nil}" -ba spec/magnum-integration.spec
-dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-plugins-2018.04-1.x86_64.rpm
-dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-plugins-devel-2018.04-1.x86_64.rpm
+dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-integration-2018.04-1.x86_64.rpm
+dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-integration-devel-2018.04-1.x86_64.rpm
+rpmbuild --clean spec/magnum-integration.spec
 
 rpmbuild --undefine=_disable_source_fetch --define "debug_package %{nil}" -ba spec/magnum-extras.spec
 dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-extras-2018.04-1.x86_64.rpm
 dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-extras-devel-2018.04-1.x86_64.rpm
+rpmbuild --clean spec/magnum-extras.spec
 
 rpmbuild --undefine=_disable_source_fetch --define "debug_package %{nil}" -ba spec/magnum-examples.spec
 dnf install -y ~/rpmbuild/RPMS/x86_64/magnum-examples-2018.04-1.x86_64.rpm
+rpmbuild --clean spec/magnum-examples.spec
